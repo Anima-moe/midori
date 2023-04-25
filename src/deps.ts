8921f2c95e3baf1e295b61693d2b53a0a38e2105
Nodgear <matheus.silva3@ftc.edu.br>
2023-04-25 16:38:38 -0300

    fix: add missing dependency for string scoring

--- src/deps.ts
+++ src/deps.ts
@@ -2,11 +2,12 @@ import {
   SqliteOrm,
   SqlTable,
 } from 'https://raw.githubusercontent.com/Blockzilla101/deno-sqlite-orm/0.5.0/mod.ts'
-import * as harmony from 'https://deno.land/x/harmony@v2.8.0/mod.ts'
+import * as harmony from 'https://raw.githubusercontent.com/harmonyland/harmony/ab841fb45a66552d844ceed914092b64d79adf51/mod.ts'
 import { crayon } from 'https://deno.land/x/crayon@3.3.3/mod.ts'
 import dayjs from 'npm:dayjs@1.11.7'
 import parser from 'https://deno.land/x/yargs_parser@yargs-parser-v21.1.1-deno/deno.ts'
 import { Cache } from 'https://deno.land/x/local_cache@1.0/mod.ts'
+import * as stringSimilarity from "https://deno.land/x/string_similarity@v1.0.1/mod.ts"
 import {
   initTranslation,
   load,
@@ -39,4 +40,5 @@ export {
   parser as argParser,
   SqlTable,
   t,
+  stringSimilarity
 }
