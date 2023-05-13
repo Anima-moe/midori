45d914aadafe86388d65870c6f9f482be186ab86
Nodgear <matheus.silva3@ftc.edu.br>
2023-05-13 06:20:48 -0300

    feat: check required env vars

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,42 @@
 import 'https://deno.land/x/dotenv@v3.2.2/load.ts'
 import * as app from '@/app.ts'
 
+const logger = new app.Logger({
+  logLevel: Deno.env.get('LOG_LEVEL') as 'info' | 'debug' | 'warn' | 'error' | 'success' || 'info',
+  prefix: 'main',
+})
+
+const expectedEnv = [
+  { name: 'BOT_OWNER', required: false },
+  { name: 'MAX_COOLDOWN', required: false },
+  { name: 'MAX_INTERACTION_TIME', required: false },
+  { name: 'ENABLE_WEBHOOKS', required: false },
+  { name: 'BOT_LOG_LEVEL', required: false },
+  { name: 'BOT_PREFIX', required: true },
+  { name: 'BOT_TOKEN', required: true },
+  { name: 'ANIMA_API', required: true },
+  { name: 'WEBHOOK_PORT', required: !!Deno.env.get('ENABLE_WEBHOOKS') },
+  { name: 'ANIMA_LOGIN', required: !!Deno.env.get('ENABLE_WEBHOOKS') },
+  { name: 'ANIMA_PASSWORD', required: !!Deno.env.get('ENABLE_WEBHOOKS') },
+]
+
+for (const env of expectedEnv) {
+  let shouldAbort = false
+
+  if (!Deno.env.get(env.name) && !env.required) {
+    logger.warn(`Missing optional environment variable: ${env.name}`)
+    continue
+  }
+
+  if (!Deno.env.get(env.name) && env.required) {
+    logger.error(`Missing required environment variable: ${env.name}`)
+    shouldAbort = true
+  }
+
+  if (shouldAbort) {
+    Deno.exit(1)
+  }
+}
 // Load modules.
 await app.database.handler.init()
 await app.command.handler.init()
