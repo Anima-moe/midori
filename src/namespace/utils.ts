d6eed8d0f1752a7e1d99621760d5530183bee087
Nodgear <matheus.silva3@ftc.edu.br>
2023-05-13 09:13:33 -0300

    feat: loginAsMidori

--- src/namespace/utils.ts
+++ src/namespace/utils.ts
@@ -1,7 +1,28 @@
+import axios from 'npm:axios@1.4.0'
+import { Anima } from "../../@types/anima.d.ts";
+
 export function splitArray<T>(arr: T[]): T[][] {
   const result: T[][] = []
   for (let i = 0; i < arr.length; i += 4) {
     result.push(arr.slice(i, i + 4))
   }
   return result
+}
+
+export async function loginAsMidori() {
+  return await axios.post<Anima.API.Login>(
+    `${Deno.env.get('ANIMA_API')}/auth/sigin`,
+    {
+      username: Deno.env.get('MIDORI_USERNAME'),
+      password: Deno.env.get('MIDORI_PASSWORD'),
+    },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      params: {
+        locale: 'en-US'
+      }
+    }
+  )
 }
